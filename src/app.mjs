c391fc13b494603b919c318a1a885933a64394f9
Matias Jimenez Salice <matias.j.salice@gmail.com>
2025-11-12 20:41:07 -0300

    ajuste en la configuración de express.json y express.urlencoded para aumentar el límite de tamaño

--- src/app.mjs
+++ src/app.mjs
@@ -36,8 +36,9 @@ app.use(cors({
   credentials: true,
 }));
 
-app.use(express.json());
-
+//app.use(express.json());
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ extended: true, limit: '20mb' }));
 
 app.use(xssSanitizer);
 
