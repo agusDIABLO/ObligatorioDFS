830defc20e897859070f5f40d743020e545a3ac9
agusDIABLO <qewad1@cloud.com>
2025-11-13 19:18:10 -0300

    userSchema sinvalidador de phone

--- src/model/schemas/user-schema.mjs
+++ src/model/schemas/user-schema.mjs
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
         required: true,
         lowercase: true, // convierte a minúsculas
         match: [/.+@.+\..+/, 'Por favor ingresa un email válido']},
-    phone: {type: Number, required: true},
+    phone: {required: true},
     password: {type: String, required: true},
     role: {type: [String], enum: ['customer', 'admin', 'barber'], default: ['customer']},
     plan: {type: String, enum: ['plus', 'premium'], default: 'plus'},
