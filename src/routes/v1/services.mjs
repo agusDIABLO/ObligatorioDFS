9e16208c50dca074495aa29f917779a2c647d196
Matias Jimenez Salice <matias.j.salice@gmail.com>
2025-11-13 01:29:30 -0300

    refactor: enable auth middleware for services route

--- src/routes/v1/services.mjs
+++ src/routes/v1/services.mjs
@@ -9,7 +9,7 @@ import { validateRoleMiddleware } from '../../middleware/validate-role-middlewar
 
 const routes = express.Router();
 
-//routes.use(authMiddleware);
+routes.use(authMiddleware);
 
 routes.post("/", validateRoleMiddleware('admin', 'customer'), validateRequest(validateCreateService, reqValidate.BODY), createService);
 routes.get("/:id", validateRoleMiddleware('admin', 'customer'), validateRequest(validateServiceById, reqValidate.PARAM), getServiceById);
