d165f64138caddf145105c2587e27334dd4073ff
Matias Jimenez Salice <matias.j.salice@gmail.com>
2025-11-13 19:30:21 -0300

    refactor: comment out redundant CORS preflight OPTIONS handling

--- src/app.mjs
+++ src/app.mjs
@@ -41,11 +41,11 @@ app.use(cors({
 }));
 
 // Ensure preflight OPTIONS requests are handled and return the CORS headers
-app.options('*', cors({
-  origin: ["http://localhost:5176", "https://obligatorio-dfs-inky.vercel.app"],
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-}));
+// app.options('*', cors({
+//   origin: ["http://localhost:5176", "https://obligatorio-dfs-inky.vercel.app"],
+//   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+// }));
 
 //app.use(express.json());
 app.use(express.json({ limit: '20mb' }));
